fabe433231cdb7145f11ae896f7e4da24ce93877
Abhishek Das <abhishekdaszy@gmail.com>
2024-10-25 19:49:48 +0530

    added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,13 @@ const page = () => {
       <div className='home-section'>
         <div className='home-container'>
           <div className='left-home'>
+            <h1> One Debt Solutions </h1>
             <p> At One Debt Solutions, we offer comprehensive debt collection services, including early-stage collections, overdue account management, and legal recovery solutions. Our expertise spans various industries, allowing us to deliver customized strategies that maximize recovery rates for our clients. With a commitment to compliance and ethical practices, we handle each case with diligence and respect. </p>
           </div>
           <div className='right-home'>
-
+            <img src='home-img.png' alt='home-img' />  
           </div>
-        </div>
+        </div> 
       </div>
     </div>
   )
